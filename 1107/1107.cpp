cdb2a19ce7366199efcd05ca9c577d508b2e812c
leafduo <leafduo@gmail.com>
2008-10-19 23:16:31 +0800

    1107 init

---
+++ 1107/1107.cpp
@@ -0,0 +1,79 @@
+#define DEBUG
+
+#include <iostream>
+#include <cstring>
+
+#ifdef DEBUG
+#include <fstream>
+using std::ifstream;
+using std::ofstream;
+ifstream fin("1107.in");
+ofstream fout("1107.out");
+#define cin fin
+#define cout fout
+#endif
+
+//using std::cin;
+//using std::cout;
+using std::endl;
+
+const int max=1000;
+bool visited[max+1][max+1];
+bool map[max+1][max+1];
+int C,R,maxDep=0;
+int startx,starty;
+
+void init() {
+    memset(visited,0,sizeof(bool)*(max+1)*(max+1));
+    memset(map,0,sizeof(bool)*(max+1)*(max+1));
+}
+
+void input() {
+    cin>>C>>R;
+    for (int i=1;i<=C;++i)
+        for (int j=1;j<=R;++j) {
+            char temp;
+            cin.get(temp);
+            if ('.'==temp)
+                map[i][j]=true;
+        }
+}
+
+void DFS(int x,int y,int depth) {
+    visited[x][y]=true;
+    startx=x;
+    starty=y;
+    if (depth>maxDep)
+        maxDep=depth;
+    if (x+1<=C&&map[x+1][y]&&visited[x][y])
+        DFS(x+1,y,depth+1);
+    if (y+1<=R&&map[x][y+1]&&visited[x][y])
+        DFS(x,y+1,depth+1);
+    if (x-1>=1&&map[x-1][y]&&visited[x][y])
+        DFS(x-1,y,depth+1);
+    if (y-1>=1&&map[x][y-1]&&visited[x][y])
+        DFS(x,y-1,depth+1);
+}
+
+int main() {
+    init();
+    input();
+    int i,j;
+    bool flag=false;
+    for (i=1;i<=C;++i)
+        for (j=1;j<=R;++j)
+            if (map[i][j]) {
+                flag=true;
+                break;
+            }
+    if (!flag) {
+        cout<<0<<endl;
+        return 0;
+    }
+    DFS(i,j,0);
+    maxDep=0;
+    memset(visited,0,sizeof(bool)*(max+1)*(max+1));
+    DFS(startx,starty,0);
+    cout<<maxDep<<endl;
+    return 0;
+}
