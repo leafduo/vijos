94d9dd96d6bd97f18c10234ceaa80f94c8864fe8
leafduo <leafduo@gmail.com>
2008-10-19 18:19:16 +0800

    1214 AC

--- 1214/1214.cpp
+++ 1214/1214.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
+#include <cmath>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
+int n;
+
+void input() {
+    cin>>n;
+}
+
+void solve() {
+    unsigned long long div=0,mod=0,first,last=n;
+    float sqrtn=sqrt(n);
+    for (int i=2;i<=sqrtn;i++) {
+        first=n/i;
+        div+=(last-first)*(i-1);
+        mod+=n*(last-first)-(first+1+last)*(last-first)/2*(i-1);
+        last=first;
+    }
+    float max=n/int(sqrtn);
+    for (int i=1;i<=max;i++){
+        div+=n/i;
+        mod+=n%i;
+    }
+    cout<<div<<" "<<mod<<endl;
+}
+
 int main() {
     for (;;) {
-        int n;
-        cin>>n;
+        input();
         if (0==n)
             break;
-        int div=0,mod=0;
-        for (int j=1;j<=n;j++) {
-            div+=n/j;
-            mod+=n%j;
-        }
-        cout<<div<<" "<<mod<<endl;
+        solve();
     }
     return 0;
 }
